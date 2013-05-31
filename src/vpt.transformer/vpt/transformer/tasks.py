0154f5ac951efeca8924c6a6bc8684b2be783894
Viet Dinh <vietdt@viu.(none)>
2013-06-01 01:22:51 +0700

    move the code that processing the import from views to celery task

--- src/vpt.transformer/vpt/transformer/tasks.py
+++ src/vpt.transformer/vpt/transformer/tasks.py
@@ -1,7 +1,79 @@
+import os
+import datetime
+import shutil
+import zipfile
+import libxml2
+import libxslt
+from lxml import etree
+from cStringIO import StringIO
+
 from celery.task import task
 
+from rhaptos.cnxmlutils.odt2cnxml import transform
+from oerpub.rhaptoslabs.cnxml2htmlpreview.cnxml2htmlpreview import cnxml_to_htmlpreview
+
+import convert as JOD # Imports JOD convert script
+
 @task
-def process_import(value):
-    time.sleep(30)
+def process_import(save_dir_path, original_filepath, filename, download_url):
     print 'processing import'
-    return 'Import result'
+
+    # convert from other office format to odt
+    odt_filename = '%s.odt' % filename
+    odt_filepath = str(os.path.join(save_dir_path, odt_filename))
+    # run jod service
+    converter = JOD.DocumentConverterClient()
+    try:
+        converter.convert(original_filepath, 'odt', odt_filepath)
+    except Exception as e:
+        raise e
+
+    # check file existed
+    try:
+        fp = open(odt_filepath, 'r')
+        fp.close()
+    except IOError as io:
+        # TODO: raise exception
+        raise io
+
+    # convert to cnxml
+    tree, files, errors = transform(odt_filepath)
+    cnxml = clean_cnxml(etree.tostring(tree))
+
+    # convert to html
+    html = cnxml_to_htmlpreview(cnxml)
+
+    # produce zipfile
+    ram = StringIO()
+    zip_archive = zipfile.ZipFile(ram, 'w')
+    # uncomment this if you need a vpxml
+    #zip_archive.writestr('index.vpxml', generateVPXML(filename, files.keys()))
+    zip_archive.writestr('index.html', html)
+    for fname, fdata in files.items():
+        zip_archive.writestr(fname, fdata)
+    zip_archive.close()
+
+    # save zipfile
+    zip_file_path = os.path.join(save_dir_path, '%s.zip' % filename)
+    if os.path.exists(zip_file_path):
+        os.rename(zip_file_path, zip_file_path + '~')
+    f = open(zip_file_path, 'wb')
+    f.write(ram.getvalue())
+    f.close()
+
+    return download_url
+
+# Pretty CNXML printing with libxml2 because etree/lxml cannot do pretty printing semantic correct
+def clean_cnxml(iCnxml, iMaxColumns=80):
+    current_dir = os.path.dirname(__file__)
+    xsl = os.path.join(current_dir, 'utils_pretty.xsl')
+    style_doc = libxml2.parseFile(xsl)
+    style = libxslt.parseStylesheetDoc(style_doc)
+    doc = libxml2.parseDoc(iCnxml)
+    result = style.applyStylesheet(doc, None)
+    pretty_cnxml = style.saveResultToString(result)
+    style.freeStylesheet()
+    doc.freeDoc()
+    result.freeDoc()
+    return pretty_cnxml
+
