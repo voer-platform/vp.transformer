59d74143d1ce366b5a362a6178e29a52b0bec498
voer <voer@dev.voer.vn>
2013-08-24 01:15:59 +0700

    set encoding=utf8 option for wkhtmltopdf

--- src/vpt.transformer/vpt/transformer/views.py
+++ src/vpt.transformer/vpt/transformer/views.py
@@ -234,7 +234,7 @@ def export_view(request):
         return Response(err_msg, 500)
     output_filename = '%s.pdf' % os.path.splitext(fs_filename)[0]
     output_file_path = os.path.join(export_dir_path, output_filename)
-    strCmd = [pdfgen, '--footer-right', '[page] / [toPage]', '--footer-spacing', '1', '-q']
+    strCmd = [pdfgen, '--footer-right', '[page] / [toPage]', '--footer-spacing', '1', '-q', '--encoding', 'utf8']
     strCmd.extend(input_file_paths)
     strCmd.append(output_file_path)
     env = { }
@@ -277,7 +277,6 @@ def getInputFiles(export_dir_path):
         with open(config_filepath, 'rb') as cf:
             lines = cf.readlines()
             data = ''.join([line.strip('\n').strip() for line in lines])
-            collection = json.loads(data)
             try:
                 collection = json.loads(data)
             except ValueError, e:
