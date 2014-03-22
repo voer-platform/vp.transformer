9e8db41fa611e0b21e500d364813fd9f2812182e
VOER <voer@voer>
2014-03-22 16:39:54 +0700

    update TOC, section titles and cleanup

--- src/vpt.transformer/vpt/transformer/tasks.py
+++ src/vpt.transformer/vpt/transformer/tasks.py
@@ -50,8 +50,6 @@ def process_import(save_dir_path, original_filepath, filename, download_url):
     # produce zipfile
     ram = StringIO()
     zip_archive = zipfile.ZipFile(ram, 'w')
-    # uncomment this if you need a vpxml
-    #zip_archive.writestr('index.vpxml', generateVPXML(filename, files.keys()))
     zip_archive.writestr('index.html', html)
     for fname, fdata in files.items():
         zip_archive.writestr(fname, fdata)
@@ -153,7 +151,7 @@ def getInputFiles(export_dir_path, save_dir_path=''):
             title = collection['title']
             editors = collection.get('editors')
             title_filepath = os.path.join(export_dir_path, 'title.html')
-            createTitlePage(title_filepath, title, editors)
+            createTitlePage(title_filepath, title, editors, save_dir_path=save_dir_path)
             # add path of title.html into result list
             results.append(title_filepath)
             # recursively process collection content
@@ -161,7 +159,7 @@ def getInputFiles(export_dir_path, save_dir_path=''):
             authors = data[2]
             # processing the title page 2
             title_filepath2 = os.path.join(export_dir_path, 'title2.html')
-            createTitlePage(title_filepath2, title, editors, authors, collection.get('url'), collection.get('version'))
+            createTitlePage(title_filepath2, title, editors, authors, collection.get('url'), collection.get('version'), save_dir_path=save_dir_path)
             # add path of title2.html into result list
             results.append(title_filepath2)
             tocs = data[1]
@@ -178,6 +176,10 @@ def getInputFiles(export_dir_path, save_dir_path=''):
             contrib_filepath = os.path.join(export_dir_path, contrib_filename)
             createContributionPage(contrib_filepath, collection, data[3])
             results.append(contrib_filepath)
+            # add end page
+            endfile_name = 'end_%s.html' % collection.get('language', 'en')
+            endfile_path = os.path.join(save_dir_path, endfile_name)
+            results.append(endfile_path)
     except IOError:
         # it's a module
         data = processModule(export_dir_path)
@@ -185,16 +187,9 @@ def getInputFiles(export_dir_path, save_dir_path=''):
         err_msg = data[1]
         extraCmd.extend(data[2])
 
-    # add footer-html with page number only if it's not set previously
-    if '--footer-html' not in extraCmd:
-        footer_filepath = os.path.join(export_dir_path, 'footer.html')
-        createHTMLFooter(footer_filepath)
-        extraCmd.extend(['--footer-html', footer_filepath])
-
     return results, err_msg, extraCmd
 
 def processModule(export_dir_path):
-    site_name = 'Th&#432; vi&#7879;n H&#7885;c li&#7879;u m&#7903; Vi&#7879;t Nam'
     results = []
     err_msg = None
     # it's a module -> return path to index.html only
@@ -222,14 +217,6 @@ def processModule(export_dir_path):
             except TypeError:
                 pass
             title = title.encode('ascii', 'xmlcharrefreplace')
-            # generate html header
-            header_filepath = os.path.join(export_dir_path, 'header.html')
-            createHTMLHeader(header_filepath, '%s - %s' % (title, site_name))
-            extraCmd.extend(['--header-html', header_filepath])
-            # add module url to footer-left
-            footer_filepath = os.path.join(export_dir_path, 'footer.html')
-            createHTMLFooter(footer_filepath, metadata.get('url', ''))
-            extraCmd.extend(['--footer-html', footer_filepath])
             # update module's index.html
             updateModuleHTML(index_filepath, metadata)
     except IOError:
@@ -249,77 +236,98 @@ def processCollection(export_dir_path, content, parents=[], save_dir_path=''):
                 id = item['id']
             else:
                 id = 'subcollection_%d_%d' % (len(parents), i)
-            # create a chapter.html file for first level only
-            chapter_filename = 'chapter_%s.html' % id
-            chapter_filepath = os.path.join(export_dir_path, chapter_filename)
-            chapter_name = item['title']
-            createChapterTitlePage(chapter_filepath, chapter_name)
-            # add path of chapter_x.html to result list
-            results.append(chapter_filepath)
         i += 1
         # build data for TOC
-        numbers = [int(n) for n in (parents + [str(i),])]
-        numbering = '.'.join(parents + [str(i),])
+        numbers = [parent.get('number', 0) for parent in parents]
+        numbers.append(i)
+        numbering = '.'.join([str(n) for n in numbers])
         toc_str = '%s. %s' % (numbering, item['title'])
         toc_level = len(parents)
-        tocs.append((toc_level, toc_str, numbers))
+        # add module id to toc to build href
+        tocs.append((toc_level, toc_str, numbers, item.get('id')))
         if item['type'] == 'module':
             authors.update(item.get('authors', []))
             modules.append(item)
             index_filepath = os.path.join(export_dir_path, item['id'], 'index.html')
             # update module's index.html
-            updateModuleHTML(index_filepath, save_dir_path=save_dir_path)
+            if len(parents) and i == 1:
+                section_titles = getParentTitles(parents)
+            else:
+                section_titles = []
+            section_titles.append([item['title'], toc_level])
+            updateModuleHTML(index_filepath, save_dir_path=save_dir_path, section_titles=section_titles)
             # add path of index.html in each module into result list
             results.append(index_filepath)
         else:
-            data = processCollection(export_dir_path, item['content'], parents + [str(i),], save_dir_path) 
+            parent = dict(
+                number = i,
+                title = item['title'],
+                level = toc_level
+            )
+            data = processCollection(export_dir_path, item['content'], parents + [parent,], save_dir_path)
             results.extend(data[0])
             tocs.extend(data[1])
             authors.update(data[2])
             modules.extend(data[3])
     return results, tocs, authors, modules
 
-def createTitlePage(filepath, title, editors=None, authors=None, url=None, version=None):
-    html = u'<html><body><h1 class="collection-title %s">%s</h1>' % (authors and 'title2' or '', title)
+def getParentTitles(parents):
+    """
+    Input: a list of parent sections of a module
+    Ouput: titles of direct parent sections (1st index in level)
+    """
+    titles = []
+    for parent in reversed(parents):
+        titles.append([parent.get('title', ''), parent.get('level', 0)])
+        if parent.get('number', 0) <> 1: break
+    return titles[::-1]
+
+def createTitlePage(filepath, title, editors=None, authors=None, url=None, version=None, save_dir_path=''):
+    # WORKAROUND: add div for title page and logo
+    #html = u'<html><body><h1 class="collection-title %s">%s</h1>' % (authors and 'title2' or '', title)
+    logo_path = os.path.join(save_dir_path, 'images/VOER.logo.jpeg')
+    html = u'<html><body><div id="title-page"><div class="logo"><center><img src="%s" width="155" /></center></div>' % logo_path
+    html += u'<h1 class="collection-title %s">%s</h1>' % (authors and 'title2' or '', title)
     # insert editors
     if editors:
         html += u"""<div id="editors">
-  <div class="by coll-by">Bi&#234;n t&#7853;p b&#7903;i:</div>"""
+  <div class="by coll-by">Edit by:</div>"""
+    # WORKAROUND
+    # <div class="by coll-by">Bi&#234;n t&#7853;p b&#7903;i:</div>"""
         for editor in editors:
             html += u'<div>%s</div>' % editor
         html += u'</div>'
     # insert authors
     if authors:
         html += u"""<div id="authors">
-  <div class="by coll-by">C&#225;c t&#225;c gi&#7843;:</div>"""
+  <div class="by coll-by">Authors:</div>"""
+    # WORKAROUND
+    # <div class="by coll-by">C&#225;c t&#225;c gi&#7843;:</div>"""
         for author in authors:
             html += u'<div>%s</div>' % author
         html += u'</div>'
     # insert link
     if url:
-        if version: url = '/'.join([url.rstrip('/'), str(version)])
+        # WORKAROUND: hide version from url
+        #if version: url = '/'.join([url.rstrip('/'), str(version)])
         html += u"""<div id="collection-link">
-  <div>Phi&#234;n b&#7843;n tr&#7921;c tuy&#7871;n:</div>"""
+  <div>Online version:</div>"""
+    # WORKAROUND
+    # <div>Phi&#234;n b&#7843;n tr&#7921;c tuy&#7871;n:</div>"""
         html += u'<div><a href="%s">%s</a></div>' % (url, url)
         html += u'</div>'
     # end html
-    html += u'</body></html>'
+    #html += u'</body></html>'
+    # WORKAROUND: add div for title page
+    html += u'</div></body></html>'
     f = codecs.open(filepath, 'wb', 'utf-8')
     f.write(html)
     f.close()
 
-def createChapterTitlePage(filepath, content):
-    try:
-        content = unicode(content, 'utf-8')
-    except TypeError:
-        pass
-    html = '<html><body><h1 class="chapter-title">%s</h1></body></html>' % content.encode('ascii', 'xmlcharrefreplace')
-    f = open(filepath, 'wb')
-    f.write(html)
-    f.close()
-
 def createTOCPage(filepath, tocs):
-    html = '<html><body><h1 id="menu">M&#7908;C L&#7908;C</h1><ul class="tocs">'
+    # WORKAROUND: en version and div for toc
+    #html = '<html><body><h1 id="menu">M&#7908;C L&#7908;C</h1><ul class="tocs">'
+    html = '<html><body><div id="toc"><h1 id="menu">Outline</h1><ul class="tocs">'
     for toc in tocs:
         toc_level = toc[0]
         toc_str = toc[1]
@@ -327,8 +335,15 @@ def createTOCPage(filepath, tocs):
             toc_str = unicode(toc_str, 'utf-8')
         except TypeError:
             pass
-        html += '<li class="level-%d">%s%s</li>' % (toc_level, '&nbsp;&nbsp;&nbsp;&nbsp;'*toc_level, toc_str.encode('ascii', 'xmlcharrefreplace'))
-    html += '<li class="level-0">Tham gia &#273;&#243;ng g&#243;p</li></ul></body></html>'
+        # TEMP: add link to module index.html file
+        #html += '<li class="level-%d">%s%s</li>' % (toc_level, '&nbsp;&nbsp;&nbsp;&nbsp;'*toc_level, toc_str.encode('ascii', 'xmlcharrefreplace'))
+        module_index_file_path = ''
+        if toc[3]:
+            module_index_file_path = '%s/index.html' % toc[3]
+        html += '<li class="level-%d">%s<a href="%s">%s</a></li>' % (toc_level, '&nbsp;&nbsp;&nbsp;&nbsp;'*toc_level, module_index_file_path, toc_str.encode('ascii', 'xmlcharrefreplace'))
+    # WORKAROUND: hide it and add div for toc
+    #html += '<li class="level-0">Tham gia &#273;&#243;ng g&#243;p</li></ul></body></html>'
+    html += '</ul></div></body></html>'
     f = open(filepath, 'wb')
     f.write(html)
     f.close()
@@ -361,75 +376,32 @@ def createContributionPage(filepath, collection, modules):
     f = codecs.open(filepath, 'wb', 'utf-8')
     f.write(html)
     f.close()
-    
-def createHTMLHeader(filepath, left_text='', right_text=''):
-    html = """
-<html><body>
-<table style="width:100%; color:grey; font-size:10pt;">
-  <tr>
-    <td>{0}</td>
-    <td style="text-align:right">{1}</td>
-  </tr>
-</table>
-</body></html>
-""".format(left_text, right_text)
-    f = open(filepath, 'wb')
-    f.write(html)
-    f.close()
 
-def createHTMLFooter(filepath, left_text='', right_text=''):
-    html = """
-<html><head><script>
-function subst() {
-  var vars={};
-  var x=document.location.search.substring(1).split('&');
-  for (var i in x) {var z=x[i].split('=',2);vars[z[0]] = unescape(z[1]);}
-  var x=['frompage','topage','page','webpage','section','subsection','subsubsection'];
-  for (var i in x) {
-    var y = document.getElementsByClassName(x[i]);
-    for (var j=0; j<y.length; ++j) y[j].textContent = vars[x[i]];
-  }
-}
-</script></head><body style="border:0; margin: 0;" onload="subst()">
-<table style="width: 100%; color:grey; font-size:10pt;">
-  <tr>
-    <td>"""
-    html += left_text
-    html += """</td>
-    <td style="text-align:right">
-      <span class="page"></span> / <span class="topage"></span>
-    </td>
-  </tr>
-</table>
-</body></html>
-"""
-    f = open(filepath, 'wb')
-    f.write(html)
-    f.close()
-
-def updateModuleHTML(filepath, metadata=None, save_dir_path=''):
+def updateModuleHTML(filepath, metadata=None, save_dir_path='', section_titles=[]):
     f = codecs.open(filepath, 'r+', 'utf-8')
     content = f.read()
     f.seek(0)
     # remove self-closing tags
     content = re.sub(r'<em[^>]+?/>', r'', content)
     content = re.sub(r'<strong[^>]+?/>', r'', content)
+    # TEMP: remove display-math.js
     html = """<html>
-<head>
-    <script type="text/javascript"
-        src="%s/display-mathml.js"></script>
-</head>
-<body>""" % save_dir_path
+<head></head>
+<body>"""
 
     if metadata:
         # insert module title and authors above content
-        html = """<h1 class="module-title">%s</h1>
+        html += """<h1 class="module-title">%s</h1>
     <div id="authors">
       <div class="by">B&#7903;i:</div>
     """ % metadata['title']
         for author in metadata.get('authors', []):
             html += '<div>%s</div>' % author
         html += '</div>'
+
+    for section_title in section_titles:
+        # insert section title (for collection export only)
+        html += """<h1 class="section-title level-%d">%s</h1>""" % (section_title[1], section_title[0])
 
     html += """%s</body></html>""" % content
     f.write(html)
